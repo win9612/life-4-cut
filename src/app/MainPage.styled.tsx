5dd6ed78c9773e4d687cc9ce57654bc3fd426f34
mingyu <64128134+win9612@users.noreply.github.com>
2024-05-05 00:19:50 +0900

    Update MainPage.styled.tsx

--- src/app/MainPage.styled.tsx
+++ src/app/MainPage.styled.tsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
-export const WrapMain = styled.div`
-
-
+export const WrapHome = styled.div`
+  display: flex;
+  flex-dirction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  width: 100%;
+  height: 100%;
 `;
