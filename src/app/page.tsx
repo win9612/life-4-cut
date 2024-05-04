5f01e758a27dc95c189005253823824060723864
mingyu <64128134+win9612@users.noreply.github.com>
2024-05-05 00:19:56 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
 import FrameNormal from "@/components/Frames/Normal";
 import WebcamViewer from "@/components/WebcamViewer";
+import { WrapHome } from "./MainPage.styled";
 
 /**
  * 홈페이지
  */
 export default function Home() {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#fff",
-        width: "100%",
-        height: "100%",
-      }}
-    >
+    <WrapHome>
       <h1
         style={{
           marginBottom: "16px",
