eb85fc660cfbee1d59b3e3b79d977330bd630c79
win9612 <mg.cho@hanwha.com>
2024-05-07 13:18:17 +0900

    add verison

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import WebcamViewer from "@/components/WebcamViewer";
 import MainPage from "@/components/template/MainPage";
+import { APP_VERSION } from "@/constants";
 
 /**
  * 홈페이지
  */
 export default function Home() {
+  console.log(APP_VERSION);
+
   return (
     <MainPage>
       <h1
